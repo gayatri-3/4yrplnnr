31ab60774cba47981cd578b71d99c37206ed1284
Gayatri Puppala <gayatri.puppala03@gmail.com>
2024-08-14 23:02:06 -0700

    created static list of courses

---
+++ src/courses.js
@@ -0,0 +1,7 @@
+export const courses = [
+    { id: "course-1", name: "Course 1" },
+    { id: "course-2", name: "Course 2" },
+    { id: "course-3", name: "Course 3" },
+    { id: "course-4", name: "Course 4" },
+    { id: "course-5", name: "Course 5" },
+];
