e6497a2d791a1a5e8a5c0ec1e473c3eff648d157
Gayatri Puppala <gayatri.puppala03@gmail.com>
2024-08-14 23:14:34 -0700

    list courses

--- src/pages/planner.js
+++ src/pages/planner.js
@@ -1,11 +1,26 @@
-import { Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import React from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { courses } from "../courses.js";
 
 export default function Planner() {
     return (
         <Flex direction="column" align="center" justify="center" p={4}>
             <Heading mb={4}>4yrplnnr</Heading>
             <Text>create your dream course plan</Text>
+
+            {/* list courses */}
+            {courses.map((course, index) => (
+                <Box
+                    mb={2}
+                    p={4}
+                    bg="white"
+                    borderWidth="1px"
+                    borderRadius="md"
+                >
+                    <Text>{course.id}</Text>
+                </Box>
+            ))}
         </Flex>
     );
 }
